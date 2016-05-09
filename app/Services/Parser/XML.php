765b010f9cebeefb9c7ff1c7d70f8fb3e48124d5
Bruno Pereira <bruno9pereira@gmail.com>
2016-05-09 00:27:25 -0300

    Add tests for XML parser

--- app/Services/Parser/XML.php
+++ app/Services/Parser/XML.php
@@ -2,31 +2,102 @@
 
 namespace App\Services\Parser;
 
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException;
+use Http\Client\Exception\RequestException;
+
+/**
+ * Class XML
+ *
+ * @package App\Services\Parser
+ * @author Bruno Pereira <bruno9pereira@gmail.com>
+ */
 class XML
 {
-    private $xml_path;
-    
-    private $xml_object;
+    /**
+     * @var string
+     */
+    private $xmlPath;
 
-    public function __construct($xml_path)
+    /**
+     * @var string
+     */
+    private $xmlContent;
+
+    /**
+     * XML constructor.
+     *
+     * @param string $xml_path
+     */
+    public function __construct(string $xml_path)
     {
-        $this->xml_path = $xml_path;
+        $this->xmlPath = $xml_path;
     }
 
+    /**
+     * Obtains XML content in array format or false if not capable
+     *
+     *@return array|boolean return array with xml content or false
+     */
     public function retrieve()
     {
-        $this->xml_object = file_get_contents($this->xml_path);
+        $this->xmlContent = $this->getContent();
+
+        if ($this->xmlContent === false) {
+            return false;
+        }
         return $this->getXMLData();
     }
 
+    /**
+     * Gets the xml content with Guzzle
+     * @return bool|string returns xml with string format or false if not possible
+     */
+    public function getContent()
+    {
+        $guzzle = new GuzzleClient();
+        try {
+            $response = $guzzle->request('GET', $this->xmlPath);
+        } catch (ClientException $Exception) {
+            return false;
+        } catch (RequestException $Exception) {
+            return false;
+        }
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * obtains xml content with array format
+     * @return array|boolean  returns an array of content or false if not possible
+     */
     private function getXMLData()
     {
-        $object = simplexml_load_string($this->xml_object, null, LIBXML_NOCDATA);
+        $object = $this->xmlStringToObject();
+
+        if ($object == false) {
+            return false;
+        }
 
         return $this->objectToArray($object);
     }
 
-    private function objectToArray($object)
+    /**
+     * Parse xml string to SimpleXMLElement object
+     * @return mixed
+     */
+    private function xmlStringToObject()
+    {
+        return simplexml_load_string($this->xmlContent, null, LIBXML_NOCDATA);
+    }
+
+    /**
+     * Parse XML Object to Array
+     * @param \SimpleXMLElement $object
+     *
+     * @return mixed
+     */
+    private function objectToArray(\SimpleXMLElement $object)
     {
         return json_decode(json_encode($object), true);
     }
