19688bd9629e2c54cc472b387411be06ca0be770
Bruno Pereira <bruno9pereira@gmail.com>
2016-05-15 21:43:02 -0300

    add total episodes field on feed table

---
+++ database/migrations/2016_05_15_203911_add_total_episodes_to_feeds_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTotalEpisodesToFeedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('feeds', function (Blueprint $table) {
+            $table->integer('total_episodes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('feeds', function (Blueprint $table) {
+            $table->dropColumn('total_episodes');
+        });
+    }
+}
