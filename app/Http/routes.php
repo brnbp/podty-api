e757ef6d23fac8956053c1a281582268fab44aae
Bruno Pereira <bruno9pereira@gmail.com>
2016-05-29 18:40:07 -0300

    add resource for queues tracking

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,23 @@ Route::get($version . '/feed/{name}', 'FeedController@retrieve');
 // search for episodes from podcast id
 Route::get($version . '/episodes/{feedId}', 'EpisodeController@retrieve');
 
+Route::get($version . '/queue', function(){
+    return DB::table('jobs')
+        ->select(['id', 'queue', 'payload', 'attempts', 'reserved'])
+        ->take(15)
+        ->get();
+});
+
+Route::get($version . '/queue/reserved', function(){
+    return DB::table('jobs')
+        ->select(['id', 'queue', 'payload', 'attempts', 'reserved'])
+        ->where('reserved', 1)
+        ->take(15)
+        ->get();
+});
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
