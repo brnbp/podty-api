c9ab5fe0203270c28e2e867d2d432f5b76b29368
Bruno Pereira <bruno9pereira@gmail.com>
2016-05-31 00:56:20 -0300

    Add QueueController class

---
+++ app/Http/Controllers/QueueController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+
+class QueueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function reserved()
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+    }
+}
