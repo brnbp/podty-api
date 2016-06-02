5ee567f1b5a384fd5d9e334f8c9ac9e2b4b26168
Bruno Pereira <bruno9pereira@gmail.com>
2016-06-01 22:19:47 -0300

    add docs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,12 +1,16 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * Class UserController
+ *
+ * @package App\Http\Controllers
+ */
 class UserController extends Controller
 {
-
+    
 }
