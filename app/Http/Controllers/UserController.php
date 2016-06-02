a979bb282a0bc1d3d90ec83e64bb518b59fa67fa
Bruno Pereira <bruno9pereira@gmail.com>
2016-06-01 22:31:09 -0300

    add method for get episodes from given user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,4 +38,35 @@ class UserController extends Controller
             ->select('feeds.id', 'feeds.name', 'feeds.thumbnail_30')
             ->get();
     }
+
+    /**
+     * Get episodes from feedId
+     * @param string $username
+     * @param integer $feedId
+     */
+    public function showEpisodes($username, $feedId)
+    {
+        return DB::table('users')
+            ->join('user_feeds', function($join) use ($feedId) {
+                $join->on('users.id', '=', 'user_feeds.user_id')
+                    ->where('user_feeds.feed_id', '=', $feedId);
+            })
+            ->join('feeds', 'user_feeds.feed_id', '=', 'feeds.id')
+            ->join('user_episodes', 'user_feeds.id','=', 'user_episodes.user_feed_id')
+            ->join('episodes', 'episodes.id', '=', 'user_episodes.id')
+            ->where('users.username', $username)
+            ->select(
+                'feeds.name as feed_name',
+                'episodes.title as episode_title',
+                'user_episodes.heard',
+                'user_episodes.hide',
+                'user_episodes.downloaded',
+                'user_episodes.paused_at',
+                'episodes.media_url',
+                'episodes.media_type',
+                'episodes.published_date',
+                'episodes.content'
+            )
+            ->get();
+    }
 }
