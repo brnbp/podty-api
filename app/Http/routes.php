33cea8accf5fa684410f0680b629e58b5dbc25d1
brnbp <bruno9pereira@gmail.com>
2016-06-13 02:57:15 -0300

    put all routes on api middleware for throttling requests

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,88 +3,79 @@
 header('Access-Control-Allow-Origin: *');
 header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
 
-$version = '/v1';
-Route::get('/', function() { return view('welcome'); });
-Route::get(
-    $version . '/',
-    function() { return view('welcome'); }
-);
-
-// search feeds that recently post new episodes
-Route::get(
-    $version . '/feeds/latest',
-    'FeedController@latest'
-);
-
-// search for podcast main informations from name
-Route::get(
-    $version . '/feeds/name/{name}',
-    'FeedController@retrieve'
-);
-
-// search for podcast main informations from feedId
-Route::get(
-    $version . '/feeds/id/{feedId}',
-    'FeedController@retrieveById'
-);
-
-// search for episodes from podcast id
-Route::get(
-    $version . '/episodes/feedId/{feedId}',
-    'EpisodeController@retrieve'
+Route::group(['middleware' => ['api']], function () {
+    $version = '/v1';
+    Route::get('/', function() { return view('welcome'); });
+    Route::get(
+        $version . '/',
+        function() { return view('welcome'); }
     );
 
-Route::get(
-    $version . '/episodes/latest',
-    'EpisodeController@latest'
-);
+    // search feeds that recently post new episodes
+    Route::get(
+        $version . '/feeds/latest',
+        'FeedController@latest'
+    );
 
-Route::post(
-    $version . '/user',
-    function(){}
-);
+    // search for podcast main informations from name
+    Route::get(
+        $version . '/feeds/name/{name}',
+        'FeedController@retrieve'
+    );
 
-Route::get(
-    $version . '/users/{username}',
-    'UserController@show'
-);
+    // search for podcast main informations from feedId
+    Route::get(
+        $version . '/feeds/id/{feedId}',
+        'FeedController@retrieveById'
+    );
 
-Route::get(
-    $version . '/users/{username}/feeds',
-    'UserController@showFeed'
-);
+    // search for episodes from podcast id
+    Route::get(
+        $version . '/episodes/feedId/{feedId}',
+        'EpisodeController@retrieve'
+    );
 
-Route::get(
-    $version . '/users/{username}/feeds/{feedId}',
-    'UserController@showEpisodes'
-);
+    Route::get(
+        $version . '/episodes/latest',
+        'EpisodeController@latest'
+    );
 
-Route::post(
-    $version . '/users/feed',
-    function(){}
-);
-Route::post(
-    $version . '/users/episodes',
-    function(){}
-);
+    Route::post(
+        $version . '/user',
+        function(){}
+    );
+
+    Route::get(
+        $version . '/users/{username}',
+        'UserController@show'
+    );
+
+    Route::get(
+        $version . '/users/{username}/feeds/latests',
+        'UserController@'
+    );
+
+    Route::get(
+        $version . '/users/{username}/episodes/latests',
+        'UserController@'
+    );
+
+    Route::get(
+        $version . '/users/{username}/feeds/{feedId}',
+        'UserController@showEpisodes'
+    );
+
+    Route::post(
+        $version . '/users/feed',
+        function(){}
+    );
+    Route::post(
+        $version . '/users/episodes',
+        function(){}
+    );
 
 
-Route::get($version . '/queue', 'QueueController@index');
-Route::delete($version . '/queue/{id}', 'QueueController@destroy');
-Route::get($version . '/queue/failed', 'QueueController@failed');
-
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
-Route::group(['middleware' => ['web']], function () {
-    //
+    Route::get($version . '/queue', 'QueueController@index');
+    Route::delete($version . '/queue/{id}', 'QueueController@destroy');
+    Route::get($version . '/queue/failed', 'QueueController@failed');
 });
