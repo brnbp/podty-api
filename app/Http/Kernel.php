cfb242312175606403bcaf7ada3f8eac00f80d67
brnbp <bruno9pereira@gmail.com>
2016-06-13 02:56:08 -0300

    remove web middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,17 +23,9 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middlewareGroups = [
-        'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-        ],
-
         'api' => [
             'throttle:60,1',
-        ],
+        ]
     ];
 
     /**
