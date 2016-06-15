0941c83cdffb6f64ccb514454ae9c4f76709db4c
brnbp <bruno9pereira@gmail.com>
2016-06-15 00:03:26 -0300

    add prefix to routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,75 +1,46 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
+header('Access-Control-Allow-Headers: Authorization, Content-Type' );
 
-Route::group(['middleware' => ['api']], function () {
-    $version = '/v1';
+Route::group(['prefix' => '/v1', 'middleware' => ['api']], function () {
     Route::get('/', function() { return view('welcome'); });
-    Route::get($version . '/', function() { return view('welcome'); });
+    Route::get('/', function() { return view('welcome'); });
 
     // search feeds that recently post new episodes
-    Route::get($version . '/feeds/latest','FeedController@latest');
+    Route::get('/feeds/latest', 'FeedController@latest');
 
     // search for podcast main informations from name
-    Route::get($version . '/feeds/name/{name}','FeedController@retrieve');
+    Route::get('/feeds/name/{name}', 'FeedController@retrieve');
 
     // search for podcast main informations from feedId
-    Route::get($version . '/feeds/id/{feedId}','FeedController@retrieveById');
+    Route::get('/feeds/id/{feedId}', 'FeedController@retrieveById');
 
     // search for episodes from podcast id
-    Route::get($version . '/episodes/feedId/{feedId}','EpisodeController@retrieve');
+    Route::get('/episodes/feedId/{feedId}', 'EpisodeController@retrieve');
 
-    Route::get($version . '/episodes/latest','EpisodeController@latest');
-
-
-
-    Route::get(
-        $version . '/users/{username}',
-        'UserController@show'
-    );
-    Route::post(
-        $version . '/user',
-        function(){}
-    );
-
-    Route::get(
-        $version . '/users/{username}/feeds',
-        'UserController@showFeed'
-    );
-
-
-    Route::get(
-        $version . '/users/{username}/feeds/{feedId}',
-        'UserController@showEpisodes'
-    );
-    Route::get(
-        $version . '/users/{username}/feeds/latests',
-        'UserController@'
-    );
-    Route::get(
-        $version . '/users/{username}/episodes/latests',
-        'UserController@'
-    );
+    Route::get('/episodes/latest', 'EpisodeController@latest');
 
 
 
-    Route::post(
-        $version . '/users/feed',
-        function(){}
-    );
+    Route::get('/users/{username}', 'UserController@show');
+    Route::post('/user', function(){});
+
+    Route::get('/users/{username}/feeds','UserController@showFeed');
 
 
-    Route::post(
-        $version . '/users/episodes',
-        function(){}
-    );
+    Route::get('/users/{username}/feeds/{feedId}', 'UserController@showEpisodes');
+    Route::get('/users/{username}/feeds/latests', 'UserController@');
+    Route::get('/users/{username}/episodes/latests', 'UserController@');
+
+
+    Route::post('/users/feed', function(){});
+
+    Route::post('/users/episodes', function(){});
 
 
 
-
-
-    Route::get($version . '/queue', 'QueueController@index');
-    Route::delete($version . '/queue/{id}', 'QueueController@destroy');
-    Route::get($version . '/queue/failed', 'QueueController@failed');
+    Route::get('/queue', 'QueueController@index');
+    Route::delete('/queue/{id}', 'QueueController@destroy');
+    Route::get('/queue/failed', 'QueueController@failed');
 });
