7fb315a3bef1ecf9b8dd6cc20ef4168a902e2baa
brnbp <bruno9pereira@gmail.com>
2016-07-09 16:56:36 -0300

    add model user with rules for validator

---
+++ app/Models/User.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $fillable = ['username', 'email', 'password'];
+
+    public static $rules = [
+        'username' => 'bail|required|alpha_num|unique:users|min:3|max:20',
+        'email' => 'bail|required|email|unique:users',
+        'password' => 'bail|required|string|min:5'
+    ];
+
+}
