85fdca632260881d71452fd7b7c72500bf1bc7ba
brnbp <bruno9pereira@gmail.com>
2016-07-09 22:13:03 -0300

    add cors middleware to be use on every request

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,8 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware\BasicAuth::class
+        \App\Http\Middleware\BasicAuth::class,
+        \App\Http\Middleware\Cors::class,
     ];
 
     /**
