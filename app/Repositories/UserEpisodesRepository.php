5a723d2a0337446c4fc10f8c6df78c7901c3eb78
brnbp <bruno9pereira@gmail.com>
2016-07-11 00:22:40 -0300

    add user episodes repository

---
+++ app/Repositories/UserEpisodesRepository.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Repositories;
+
+class UserEpisodesRepository
+{
+    public static function all()
+    {
+    }
+
+    public static function one()
+    {
+    }
+
+    public static function first()
+    {
+    }
+
+    public static function create()
+    {
+    }
+
+    public static function batchCreate()
+    {
+    }
+
+    public static function delete()
+    {
+    }
+}
