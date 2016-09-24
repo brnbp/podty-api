60d7da0d460e65348e4bed1f08786bb77b05c29f
brnbp <bruno9pereira@gmail.com>
2016-09-24 17:31:24 -0300

    create XML transformer class

---
+++ app/Transform/XMLTransformer.php
@@ -0,0 +1,70 @@
+<?php
+namespace App\Transform;
+
+/**
+ * Class XMLTransformer
+ *
+ * @package App\Transform
+ */
+class XMLTransformer extends TransformerAbstract
+{
+    /**
+     * Transforma um feed para um retorno padrao
+     * @param $feed
+     *
+     * @return array
+     */
+    public function transform($feedXML)
+    {
+        $episodes = [];
+        foreach ($feedXML->channel->item as $entry) {
+
+            $nsElements = $this->getNamespaceChildren($entry);
+
+            $enclosure = $this->getEnclosureAttributes($entry);
+
+            $episodes[] = [
+                'title' => array_first($entry->title),
+                'link' => array_first($entry->link) ?? '',
+                'published_date' => array_first($entry->pubDate) ?? '',
+                'content' => array_first($entry->description) ?? '',
+
+                'summary' => array_first($nsElements->summary),
+                'image' => $this->getImageUrl($nsElements),
+                'duration' => array_first($nsElements->duration),
+
+                'media_url' => $enclosure['@attributes']['url'] ?? '',
+                'media_length' => $enclosure['@attributes']['length'] ?? 0,
+                'media_type' => $enclosure['@attributes']['type'] ?? 'audio/mp3',
+            ];
+
+        }
+
+        return $episodes;
+    }
+
+    private function getNamespaceChildren($entry)
+    {
+        $namespaces = $entry->getNameSpaces(true);
+
+        return $entry->children($namespaces['itunes']);
+    }
+
+    private function getImageUrl($nsElements)
+    {
+        if (!isset($nsElements->image)) {
+            return '';
+        }
+
+        if (!isset($nsElements->image->attributes()->href)) {
+            return '';
+        }
+
+        return reset($nsElements->image->attributes()->href);
+    }
+
+    private function getEnclosureAttributes($entry)
+    {
+        return json_decode(json_encode($entry->enclosure->attributes()), true);
+    }
+}
