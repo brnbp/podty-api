8dbd81243c4658a9707cc1c03edf5c346152027e
brnbp <bruno9pereira@gmail.com>
2016-09-24 13:30:05 -0300

    add columns to episode table

---
+++ database/migrations/2016_09_24_161409_add_fields_episode_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsEpisodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('episodes', function ($table) {
+            $table->string('image')->nullable()->after('content');;
+            $table->string('duration', 50)->nullable()->after('image');
+            $table->string('summary')->nullable()->after('published_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('episodes', function ($table) {
+            $table->dropColumn(['image', 'duration', 'summary']);
+        });
+    }
+}
