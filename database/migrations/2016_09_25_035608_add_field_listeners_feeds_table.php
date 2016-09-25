5b50c83c433e17b74a71c6ce39b32dcdcbdd0b3d
brnbp <bruno9pereira@gmail.com>
2016-09-25 03:22:06 -0300

    add field listeners to feeds table

---
+++ database/migrations/2016_09_25_035608_add_field_listeners_feeds_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldListenersFeedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('feeds', function ($table) {
+            $table->integer('listeners')->nullable()->default(0)->after('last_episode_at');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('feeds', function ($table) {
+            $table->dropColumn('listeners');
+        });
+    }
+}
