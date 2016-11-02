af8ccb479496c5c77f5e9df868e32b84bee26ac0
Ernando Souza <ernandosouza04@gmail.com>
2016-11-02 00:18:12 -0200

    UsersTable model factory with seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,10 @@
 <?php
+
 use Illuminate\Database\Seeder;
 
+/**
+ * Class UsersTableSeeder
+ */
 class UsersTableSeeder extends Seeder
 {
 
@@ -11,27 +15,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            [
-                'username' => 'bruno2546',
-                'email' => 'bruno2546@brnbp.me',
-                'password' => bcrypt('bruno2546'),
-                'remember_token' => md5('bruno2546'),
-                'friends_count' => 1,
-                'podcasts_count' => 1,
-                'created_at' => \Carbon\Carbon::now(),
-                'updated_at' => \Carbon\Carbon::now()
-            ],
-            [
-                'username' => 'brnbp',
-                'email' => 'brnbp@brnbp.me',
-                'password' => bcrypt('brnbp'),
-                'remember_token' => md5('brnbp'),
-                'friends_count' => 1,
-                'podcasts_count' => 1,
-                'created_at' => \Carbon\Carbon::now(),
-                'updated_at' => \Carbon\Carbon::now()
-            ]
-        ]);
+        factory(\App\Models\User::class, 2)->create();
     }
 }
