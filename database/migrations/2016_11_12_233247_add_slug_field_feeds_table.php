9ffb42bb939eb57dcb4eaa2ad21b27b2c86905de
brnbp <bruno9pereira@gmail.com>
2016-11-12 23:01:51 -0200

    create migration for slug field on feeds table📑

---
+++ database/migrations/2016_11_12_233247_add_slug_field_feeds_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSlugFieldFeedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('feeds', function ($table) {
+            $table->string('slug', 50)->nullable()->after('name');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('feeds', function ($table) {
+            $table->dropColumn('slug');
+        });
+    }
+}
