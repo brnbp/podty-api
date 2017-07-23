23e191670fe5adb0062a02cf9df4b202e05294bd
brnbp <bruno9pereira@gmail.com>
2017-07-23 19:31:34 -0300

    rollback last commit

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -19,7 +19,7 @@ class UserRepository
         $user = new User([
             'username' => $userData['username'],
             'email' => $userData['email'],
-            'password' => $userData['password'],
+            'password' => Password::encrypt($userData['password']),
             'friends_count' => 0,
             'podcasts_count' => 0,
         ]);
