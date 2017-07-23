a36b2aedbddcbea0555befed5489520fe810df4a
brnbp <bruno9pereira@gmail.com>
2017-07-23 07:00:35 -0300

    tests for user delete

---
+++ tests/Integration/Users/DeleteUserTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace Tests\Integration\Users;
+
+use App\Models\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class DeleteUserTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    /** @test */
+    public function unauthenticated_client_cannot_delete_user()
+    {
+        factory(User::class)->make();
+
+        $this->delete('v1/users/username')
+                ->assertResponseStatus(401);
+    }
+    
+    /** @test */
+    public function it_deletes_an_user()
+    {
+        $this->authenticate();
+        
+        $user = factory(User::class)->create();
+        
+        $this->delete('v1/users/' . $user->username)
+            ->seeText(json_encode(['removed' => true]))
+            ->assertResponseStatus(200);
+        
+        $this->get('v1/users/' . $user->username)
+            ->assertResponseStatus(404);
+    }
+    
+    /** @test */
+    public function it_requires_a_valid_user()
+    {
+        $this->authenticate();
+        
+        factory(User::class, 3)->create();
+        
+        $this->delete('v1/users/not-valid-user')
+            ->assertResponseStatus(404);
+    }
+}
