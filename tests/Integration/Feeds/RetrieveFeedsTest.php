65ffeaa9073bca588f646416f4b8508d92049173
brnbp <bruno9pereira@gmail.com>
2017-07-29 17:56:11 -0300

    separe tests for retrieving feeds

--- tests/Integration/Feeds/RetrieveFeedsTest.php
+++ tests/Integration/Feeds/RetrieveFeedsTest.php
@@ -6,7 +6,6 @@ use App\Models\Feed;
 use App\Models\User;
 use App\Repositories\UserFeedsRepository;
 use App\Transform\FeedTransformer;
-use App\Transform\UserTransformer;
 use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -67,18 +66,6 @@ class RetrieveFeedsTest extends TestCase
         ]);
         
         $this->get('/v1/feeds/top')
-            ->seeStatusCode(401);
-    }
-    
-    /** @test */
-    public function unauthenticated_client_cannot_retrieve_listeners_for_feed()
-    {
-        $feed = factory(Feed::class)->create();
-        $user = factory(User::class)->create();
-        
-        UserFeedsRepository::create($feed->id, $user);
-        
-        $this->get('/v1/feeds/1/listeners')
             ->seeStatusCode(401);
     }
     
@@ -221,49 +208,6 @@ class RetrieveFeedsTest extends TestCase
             ->seeStatusCode(404);
     }
     
-    /** @test */
-    public function it_retrieves_listeners_for_feed()
-    {
-        $this->authenticate();
-        
-        $feed = factory(Feed::class)->create();
-        
-        $users = factory(User::class, 2)->create();
-        
-        $users->each(function($user) use($feed) {
-            UserFeedsRepository::create($feed->id, $user);
-        });
-        
-        $this->get('/v1/feeds/1/listeners')
-            ->seeJsonEquals([
-                'data' => [
-                    (new UserTransformer())->transform($users->first()->fresh()),
-                    (new UserTransformer())->transform($users->last()->fresh()),
-                ]
-            ])
-            ->seeStatusCode(200);
-    }
-    
-    /** @test */
-    public function it_returns_404_when_retrieving_listeners_for_feeds_having_no_listeners()
-    {
-        $this->authenticate();
-        
-        factory(Feed::class)->create();
-        
-        $this->get('/v1/feeds/1/listeners')
-            ->seeStatusCode(404);
-    }
-    
-    /** @test */
-    public function it_returns_404_when_retrieving_listeners_for_not_found_feed()
-    {
-        $this->authenticate();
-        
-        $this->get('/v1/feeds/1/listeners')
-            ->seeStatusCode(404);
-    }
-    
     private function getDefaultStructure()
     {
         return [
@@ -288,5 +232,4 @@ class RetrieveFeedsTest extends TestCase
             'last_episode_at'
         ];
     }
-
 }
