56a9c510b4e8e2703d7f43dde74d4d161c80adee
brnbp <bruno9pereira@gmail.com>
2017-07-30 23:48:06 -0300

    add tests for retriving episodes from feed

---
+++ tests/Integration/Episodes/RetrieveFeedEpisodesTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace Tests\Integration\Episodes;
+
+use App\Models\Episode;
+use App\Models\Feed;
+use App\Transform\EpisodeTransformer;
+use App\Transform\FeedTransformer;
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RetrieveFeedEpisodesTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    /** @test */
+    public function unauthenticated_client_cannot_retrieve_latests_episode()
+    {
+        $this->get('/v1/feeds/1/episodes')
+            ->seeStatusCode(401);
+    }
+    
+    /** @test */
+    public function it_retrieves_episodes_from_feed()
+    {
+        $this->authenticate();
+        
+        $feed = factory(Feed::class)->create();
+        
+        $episodes = factory(Episode::class)->create([
+            'feed_id' => $feed->id,
+        ]);
+        
+        $response = $this->get('/v1/feeds/1/episodes')
+                        ->seeStatusCode(200);
+    
+        $response = json_decode($response->response->getContent(), true);
+    
+        $expected = (new FeedTransformer)->transform($feed);
+        $expected['episodes'][] = (new EpisodeTransformer())->transform($episodes);
+
+        $this->assertEquals([
+            'data' => $expected
+        ], $response);
+    }
+    
+    /** @test */
+    public function it_returns_404_when_retrieving_unexistent_episodes_from_given_feed()
+    {
+        $this->authenticate();
+        
+        factory(Feed::class)->create();
+
+        $this->get('/v1/feeds/1/episodes')
+            ->seeStatusCode(404);
+    }
+    
+    /** @test */
+    public function it_returns_404_when_retrieving_episodes_from_unexistent_feed()
+    {
+        $this->authenticate();
+        
+        $this->get('/v1/feeds/1/episodes')
+            ->seeStatusCode(404);
+    }
+}
+
