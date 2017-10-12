e118fe05ef815e0d2c5133628e32fe84a48fd177
brnbp <bruno9pereira@gmail.com>
2017-10-12 16:12:17 -0300

    refactor ratings using eloquent relations and move calcule to rating model

--- app/Listeners/RecalculateRating.php
+++ app/Listeners/RecalculateRating.php
@@ -15,23 +15,10 @@ class RecalculateRating implements ShouldQueue
      */
     public function handle(ContentRated $event)
     {
-        $ratings = Rating::where('content_id', $event->content->id)
-                ->where('content_type', $event->contentType)
-                ->get();
-        
-        
-        $average = $ratings->avg('rate');
-        
-        $event->content->avg_rating = $this->roundToHalfDecimalOrDecimal($average);
-        
+        $average = $event->content->ratings->avg('rate');
+
+        $event->content->avg_rating = Rating::round($average);
+
         $event->content->save();
-    }
-    
-    /*
-     * rounds number to 0.50, 1.00, 1.50, 2.00, 2.50 and so on
-     */
-    private function roundToHalfDecimalOrDecimal($number)
-    {
-        return round($number * 2) / 2;
     }
 }
