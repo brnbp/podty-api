3e1f2ae7fbe00d759edb2d8d79d013186b34619f
brnbp <bruno9pereira@gmail.com>
2017-10-12 23:32:16 -0300

    change static methods to non-static

--- app/Http/Controllers/v1/UserFavoriteController.php
+++ app/Http/Controllers/v1/UserFavoriteController.php
@@ -8,9 +8,19 @@ use App\Repositories\UserFavoritesRepository;
 
 class UserFavoriteController extends ApiController
 {
+    /**
+     * @var \App\Repositories\UserFavoritesRepository $repository
+     */
+    private $repository;
+
+    public function __construct(UserFavoritesRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function all(User $user)
     {
-        $favorites = UserFavoritesRepository::all($user);
+        $favorites = $this->repository->all($user);
 
         if (!$favorites->count()) {
             return $this->respondNotFound();
@@ -21,14 +31,14 @@ class UserFavoriteController extends ApiController
 
     public function favorite(User $user, Episode $episode)
     {
-        UserFavoritesRepository::create($user, $episode);
+        $this->repository->create($user, $episode);
 
         return $this->respondCreated();
     }
 
     public function unfavorite(User $user, Episode $episode)
     {
-        $userEpisodeFavorite = UserFavoritesRepository::delete($user, $episode);
+        $userEpisodeFavorite = $this->repository->delete($user, $episode);
 
         if ($userEpisodeFavorite) {
             return $this->respondSuccess(['deleted' => true]);
