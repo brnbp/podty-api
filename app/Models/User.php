9dfa0001578aa638079f5225f6ae831ddff68323
brnbp <bruno9pereira@gmail.com>
2017-10-26 19:22:07 -0200

    move welcome email for new users to model created event

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Models;
 
+use App\Mail\UserRegistered;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Mail;
 
 class User extends Model
 {
@@ -14,6 +16,15 @@ class User extends Model
         'email' => 'bail|required|email|unique:users',
         'password' => 'bail|required|string|min:5',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function (User $user) {
+            Mail::send(new UserRegistered($user));
+        });
+    }
 
     public function feeds()
     {
