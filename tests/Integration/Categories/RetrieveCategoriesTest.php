9dd0e8c48f6ff5bada20915fcfff9b2a2f449afd
brnbp <bruno9pereira@gmail.com>
2017-10-30 15:01:03 -0200

    create test class

---
+++ tests/Integration/Categories/RetrieveCategoriesTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RetrieveCategoriesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function it_()
+    {
+
+    }
+}
